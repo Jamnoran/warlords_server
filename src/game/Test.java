7484a7fa3d07006e16af711caad9b9ac0be494fb
jamnoran <lindahl.eric@gmail.com>
2016-06-29 22:04:18 +0200

    Added test

---
+++ src/game/Test.java
@@ -0,0 +1,28 @@
+package game;
+
+import game.logging.Log;
+import vo.Message;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+
+public class Test {
+
+	private static final String TAG = Test.class.getSimpleName();
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Log.i(TAG, "Saving information to firebase!");
+		String classType = "WARRIOR";
+		String user = "Emil";
+
+		// DO stuff
+
+	}
+
+}
