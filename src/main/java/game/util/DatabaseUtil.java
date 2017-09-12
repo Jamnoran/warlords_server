de6b678653f7f6c76342a63e6acae26ca0c5bdc5
ErCa <lindahl.eric@gmail.com>
2017-09-12 23:06:37 +0200

    Changed database url

--- src/main/java/game/util/DatabaseUtil.java
+++ src/main/java/game/util/DatabaseUtil.java
@@ -14,8 +14,8 @@ import java.util.ArrayList;
  */
 public class DatabaseUtil {
 	private static final String TAG = DatabaseUtil.class.getSimpleName();
-//	private static String ip = "warlord.ga";
-	private static String ip = "192.168.0.215";
+	private static String ip = "warlord.ga";
+//	private static String ip = "192.168.0.215";
 	private static String port = "8889";
 	private static String user = "warlord_clients";
 	private static String password = "bosse45&";
