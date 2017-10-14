62af564d2eb2f988dedbb608980e03dac87a1041
Jamnoran <lindahl.eric@gmail.com>
2017-10-14 19:16:35 +0200

    Changed port

--- src/main/java/game/io/WebserviceCommunication.java
+++ src/main/java/game/io/WebserviceCommunication.java
@@ -20,7 +20,7 @@ import java.util.ArrayList;
  */
 public class WebserviceCommunication {
 	private static final String TAG = WebserviceCommunication.class.getSimpleName();
-	private static String  registerServer = "http://www.warlord.ga/warlords_webservice/register_server.php";
+	private static String  registerServer = "http://www.warlord.ga:8000/warlords_webservice/register_server.php";
 	private static final String USER_AGENT = "Mozilla/5.0";
 	private static String ip = "2.248.122.35";
 //	private static String ip = "192.168.0.207";
