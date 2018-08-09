fbd62a66a3106bb52c6a5c0ed1d20a9592d11eb6
Eric Lindahl <eric.lindahl@payair.com>
2018-08-09 14:27:12 +0200

    Removed test

--- src/main/java/test/PriestHealOverTimeTest.java
+++ src/main/java/test/PriestHealOverTimeTest.java
@@ -14,34 +14,34 @@ import java.util.Collections;
 
 public class PriestHealOverTimeTest {
 
-	@org.junit.Test
-	public void castTimeCompleted() {
-
-		long time = System.currentTimeMillis();
-		Hero hero = DatabaseUtil.getHero(13);
-		Priest priest = (Priest) hero;
-
-		priest.generateHeroInformation();
-
-		ArrayList<Ability> heroAbilities = DatabaseUtil.getAllAbilities(hero.getClass_type());
-		ArrayList<AbilityPosition> abilityPositions = DatabaseUtil.getHeroAbilityPositions(hero.getId());
-		for (AbilityPosition abilityPosition : abilityPositions) {
-			for (Ability ability : heroAbilities) {
-				if (abilityPosition.getAbilityId() == ability.getId()) {
-					ability.setPosition(abilityPosition.getPosition());
-				}
-			}
-		}
-
-		Collections.sort(heroAbilities, new Ability());
-
-		priest.setAbilities(heroAbilities);
-
-		GameServer gameServer = new GameServer(null);
-		ArrayList<Integer> targetFriendly = new ArrayList<>();
-		targetFriendly.add(priest.getId());
-		Vector3 position = new Vector3(0,0,0);
-		PriestHealOverTime spell = new PriestHealOverTime( time,  hero,  priest.getAbility(5), gameServer,  null,  targetFriendly,  position);
-		spell.execute();
-	}
+//	@org.junit.Test
+//	public void castTimeCompleted() {
+//
+//		long time = System.currentTimeMillis();
+//		Hero hero = DatabaseUtil.getHero(13);
+//		Priest priest = (Priest) hero;
+//
+//		priest.generateHeroInformation();
+//
+//		ArrayList<Ability> heroAbilities = DatabaseUtil.getAllAbilities(hero.getClass_type());
+//		ArrayList<AbilityPosition> abilityPositions = DatabaseUtil.getHeroAbilityPositions(hero.getId());
+//		for (AbilityPosition abilityPosition : abilityPositions) {
+//			for (Ability ability : heroAbilities) {
+//				if (abilityPosition.getAbilityId() == ability.getId()) {
+//					ability.setPosition(abilityPosition.getPosition());
+//				}
+//			}
+//		}
+//
+//		Collections.sort(heroAbilities, new Ability());
+//
+//		priest.setAbilities(heroAbilities);
+//
+//		GameServer gameServer = new GameServer(null);
+//		ArrayList<Integer> targetFriendly = new ArrayList<>();
+//		targetFriendly.add(priest.getId());
+//		Vector3 position = new Vector3(0,0,0);
+//		PriestHealOverTime spell = new PriestHealOverTime( time,  hero,  priest.getAbility(5), gameServer,  null,  targetFriendly,  position);
+//		spell.execute();
+//	}
 }
